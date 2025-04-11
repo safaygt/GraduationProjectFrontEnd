0111d5f83fc48db48f5239724889bee874346e04
safa <safayigit40@gmail.com>
2025-04-11 18:09:15 +0300

    some design changed for dashboard page

--- graduation/src/layouts/Footer.jsx
+++ graduation/src/layouts/Footer.jsx
@@ -3,9 +3,18 @@ import '../assets/css/Footer.css'; // CSS dosyasını içe aktar
 
 function Footer() {
     return (
-        <footer>
-            <p>&copy; {new Date().getFullYear()} SAFA YİĞİT</p>
-        </footer>
+        <div className="container">
+            <footer className="py-3 my-4">
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
+                </ul>
+                <p className="text-center text-body-secondary">&copy; {new Date().getFullYear()} SAFA YİĞİT</p>
+            </footer>
+        </div>
     );
 }
 
--- graduation/src/pages/Dashboard.jsx
+++ graduation/src/pages/Dashboard.jsx
@@ -28,20 +28,13 @@ function Dashboard() {
         setResult(null);
         setProgress(0);
 
-        // input’un aynı dosyayı yeniden seçebilmesini sağla
         if (fileInputRef.current) {
             fileInputRef.current.value = '';
         }
     };
 
-
-
-
-
     useEffect(() => {
-
         toast.dismiss();
-
 
         const user = AuthService.getCurrentUser();
         if (!user) {
@@ -99,7 +92,6 @@ function Dashboard() {
         }));
     };
 
-
     const recycleItems = [
         {
             id: 'kagit',
@@ -129,69 +121,73 @@ function Dashboard() {
 
     return (
         <div className="dashboard-container">
-            <div className="geri-donusum-bilgileri">
-                {recycleItems.map(item => (
-                    <div className="geri-donusum-item" key={item.id}>
-                        <img src={item.image} alt={item.title} className="geri-donusum-resim" />
-                        <h3>{item.title}</h3>
-                        <p>
-                            {expanded[item.id] ? item.description : `${item.description.substring(0, 100)}...`}
-                        </p>
-                        {item.description.length > 100 && (
-                            <div className="expand-button-container">
-                                <button className="expand-button" onClick={() => toggleExpand(item.id)}>
-                                    {expanded[item.id] ? '▲ Daha Az Göster' : '▼ Daha Fazla Göster'}
-                                </button>
-                            </div>
-                        )}
-                    </div>
-                ))}
+            <div className="geri-donusum-bilgileri-container">
+                <div className="geri-donusum-bilgileri">
+                    {recycleItems.map(item => (
+                        <div className="geri-donusum-item" key={item.id}>
+                            <img src={item.image} alt={item.title} className="geri-donusum-resim" />
+                            <h3>{item.title}</h3>
+                            <p>
+                                {expanded[item.id] ? item.description : `${item.description.substring(0, 100)}...`}
+                            </p>
+                            {item.description.length > 100 && (
+                                <div className="expand-button-container">
+                                    <button className="expand-button" onClick={() => toggleExpand(item.id)}>
+                                        {expanded[item.id] ? '▲ Daha Az Göster' : '▼ Daha Fazla Göster'}
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div className="resim-yukleme-alani">
-                <button onClick={handleButtonClick}>Dosya Seç</button>
-                <input
-                    type="file"
-                    accept="image/*"
-                    onChange={handleFileChange}
-                    ref={fileInputRef}
-                    style={{ display: "none" }}
-                />
-                <button onClick={handleUpload}>Hesapla</button>
-                {selectedFile && (
-                    <div className="file-info-container">
-                        <p className="file-name">{selectedFile.name}</p>
-                        <button className="remove-file-button" id="deleteButton" onClick={handleRemoveFile}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
-                                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
-                            </svg>
-                        </button>
+            <div className="resim-yukleme-alani-container">
+                <div className="resim-yukleme-alani">
+                    <button onClick={handleButtonClick}>Dosya Seç</button>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={handleFileChange}
+                        ref={fileInputRef}
+                        style={{ display: "none" }}
+                    />
+                    <button onClick={handleUpload}>Hesapla</button>
+                    {selectedFile && (
+                        <div className="file-info-container">
+                            <p className="file-name">{selectedFile.name}</p>
+                            <button className="remove-file-button" id="deleteButton" onClick={handleRemoveFile}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                                </svg>
+                            </button>
+                        </div>
+                    )}
+                </div>
+
+                {loading && (
+                    <div className="progress-bar-container">
+                        <div className="progress-bar" style={{ width: `${progress}%` }}></div>
+                    </div>
+                )}
+
+                {result && (
+                    <div className="tahmin-sonucu">
+                        <h3>Tahmin Sonucu</h3>
+                        {result.detections && result.detections.length > 0 ? (
+                            <div>
+                                {result.class_counts && Object.entries(result.class_counts).map(([className, count]) => (
+                                    <p key={className}>{className}: {count} adet dönüştürdünüz!</p>
+                                ))}
+                                {result.total_effect && (
+                                    <p>Bu geri dönüşümle dünyaya %{result.total_effect.toFixed(4)} katkı verdiniz.</p>
+                                )}
+                            </div>
+                        ) : (
+                            <p>Herhangi bir geri dönüştürülebilir malzeme bulunamadı.</p>
+                        )}
                     </div>
                 )}
             </div>
-
-            {loading && (
-                <div className="progress-bar-container">
-                    <div className="progress-bar" style={{ width: `${progress}%` }}></div>
-                </div>
-            )}
-
-            {result && (
-                <div className="tahmin-sonucu">
-                    <h3>Tahmin Sonucu</h3>
-                    {result.detections && result.detections.length > 0 ? (
-                        <div>
-                            {result.class_counts && Object.entries(result.class_counts).map(([className, count]) => (
-                                <p key={className}>{className}: {count} adet dönüştürdünüz!</p>
-                            ))}
-                            {result.total_effect && (
-                                <p>Bu geri dönüşümle dünyaya %{result.total_effect.toFixed(4)} katkı verdiniz.</p>
-                            )}
-                        </div>
-                    ) : (
-                        <p>Herhangi bir geri dönüştürülebilir malzeme bulunamadı.</p>
-                    )}
-                </div>
-            )}
         </div>
     );
 }
